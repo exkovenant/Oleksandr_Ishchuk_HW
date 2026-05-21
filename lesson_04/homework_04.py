08472ed479a648b665c4f12b3f931c1988750ae9
Oleksandr Ishchuk <exkovenant@gmail.com>
2026-05-21 18:13:07 +0300

    Add homework 04

---
+++ lesson_04/homework_04.py
@@ -0,0 +1,62 @@
+# task 1
+adwentures_of_tom_sawer = """\
+Tom gave up the brush with reluctance in his .... face but alacrity
+in his heart. And while
+the late steamer
+"Big Missouri" worked ....
+and sweated
+in the sun,
+the retired artist sat on a barrel in the .... shade close by, dangled his legs,
+munched his apple, and planned the slaughter of more innocents.
+There was no lack of material;
+boys happened along every little while;
+they came to jeer, but .... remained to whitewash. ....
+By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
+a kite, in good repair;
+and when he played
+out, Johnny Miller bought
+in for a dead rat and a string to swing it with—and so on, and so on,
+hour after hour. And when the middle of the afternoon came, from being a
+poor poverty, stricken boy in the .... morning, Tom was literally
+rolling in wealth."""
+
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
+print(adwentures_of_tom_sawer)
+
+# task 2
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
+print(adwentures_of_tom_sawer)
+
+# task 3
+adwentures_of_tom_sawer = " ".join(adwentures_of_tom_sawer.split())
+print(adwentures_of_tom_sawer)
+
+# task 4
+print(adwentures_of_tom_sawer.count("h"))
+
+# task 5
+count = 0
+for word in adwentures_of_tom_sawer.split():
+    if word[0].isupper():
+        count += 1
+print(count)
+
+# task 6
+first = adwentures_of_tom_sawer.find("Tom")
+second = adwentures_of_tom_sawer.find("Tom", first + 1)
+print(second)
+
+# task 7
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split(".")
+print(adwentures_of_tom_sawer_sentences)
+
+# task 8
+print(adwentures_of_tom_sawer_sentences[3].lower())
+
+# task 9
+for sentence in adwentures_of_tom_sawer_sentences:
+    if sentence.strip().startswith("By the time"):
+        print(True)
+
+# task 10
+print(len(adwentures_of_tom_sawer_sentences[-2].split()))
